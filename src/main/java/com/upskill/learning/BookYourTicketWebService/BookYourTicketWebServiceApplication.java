bd78244b8b74878ea391b0af4622c9bab9dcdb5f
seemanthini sathi <seemanthinisathi@seemanthinis-MacBook-Pro.local>
2022-08-06 10:12:25 +0530

    initial commit

---
+++ src/main/java/com/upskill/learning/BookYourTicketWebService/BookYourTicketWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upskill.learning.BookYourTicketWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYourTicketWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYourTicketWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upskill/learning/BookYourTicketWebService/BookYourTicketWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upskill.learning.BookYourTicketWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYourTicketWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
